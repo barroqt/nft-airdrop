542cb6a70e6c53b011dbc5053b2115cb187e02cd
barroqt <barroqt@gmail.com>
2022-02-06 23:18:06 +0100

    add minting and setup addresses

--- contracts/airdrop.sol
+++ contracts/airdrop.sol
@@ -1,2 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+contract Airdrop {
+
+    mapping(uint => address) public nftToOwner;
+
+    function airdropNFT(address[] memory _recipients) external {
+        for(uint i = 0; i < _recipients.length; i++) {
+
+        }
+    }
+
+    // whitelist owners
+
+    // claim
+}
--- contracts/nft.sol
+++ contracts/nft.sol
@@ -1,2 +1,10 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract NFT is ERC721 {
+    constructor() public ERC721("MyNft", "NFT") {
+        _safeMint(msg.sender, 0);
+    }
+}
