57a0c1c161b2760ec94bad18c500e759dd3ff789
barroqt <barroqt@gmail.com>
2022-02-07 18:06:06 +0100

    transfer nft to recipient

--- contracts/airdrop.sol
+++ contracts/airdrop.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Airdrop {
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
+contract Airdrop {
+    uint public tokenId;
     mapping(uint => address) public nftToOwner;
 
     function airdropNFT(address[] memory _recipients) external {
         for(uint i = 0; i < _recipients.length; i++) {
-
+            nftToOwner[tokenId] = _recipients[i];
+            IERC721(_recipients[i]).transferFrom(msg.sender, _recipients[i], tokenId);
         }
     }
 
